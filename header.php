b47b58d2f5256e0dacf82426b05800e1adb42ba0
robertdev18 <robertdev18@gmail.com>
2025-02-05 20:04:41 -0500

    v1 psr4 fix 5 namespace class

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <?php 
-ini_set('display_errors','1'); 
+ini_set('display_errors','1');
+ini_set('display_startup_error','1');
+error_reporting(E_ALL);
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes();?>>
--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
 <?php the_title(); ?>
-<?php echo Furry\Categories::current_category(); >
+<?php echo Furry\\Categories::current_category(); >
 <?php the_content(); ?>
 <?php get_footer(); ?>
