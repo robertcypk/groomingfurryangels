281aadbbc9925a7815c8152da28bbed0e33a471b
robertdev18 <robertdev18@gmail.com>
2025-02-05 21:29:46 -0500

    v1 fix single

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php ini_set('display_errors','1'); ?>
 <?php wp_header(); ?>
 <?php echo the_title(); ?>
-<?php echo Furry\\Categories::current_category(); >
+<?php echo Furry\\Categories::current_category(); ?>
 <?php echo the_content(); ?>
 <?php wp_footer(); ?>
