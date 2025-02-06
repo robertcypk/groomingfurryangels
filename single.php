62850ab86adc3f0464acb20973cf07c390ae3105
robertdev18 <robertdev18@gmail.com>
2025-02-05 20:12:41 -0500

    v1 psr4 fix 7 namespace class

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php ini_set('display_errors','1'); ?>
 <?php get_header(); ?>
-<?php the_title(); ?>
+<?php echo the_title(); ?>
 <?php echo Furry\\Categories::current_category(); >
-<?php the_content(); ?>
+<?php echo the_content(); ?>
 <?php get_footer(); ?>
