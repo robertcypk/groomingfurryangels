d69f23d6d0bbf5db1f6a7dd4d50838d3d0487715
robertdev <robertdev18@gmail.com>
2025-02-10 23:12:59 -0500

    class categorie

--- includes/categories.php
+++ includes/categories.php
@@ -7,7 +7,9 @@ class Categories{
     add_action('init',[$this,'register_categories']);
   }
 
-  public function register_categories(){}
+  public function register_categories(){
+    return 'Categories Class';
+  }
 
   public static function get_instance(){
     if(null == self:$instance){
