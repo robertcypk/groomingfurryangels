e30493ff6fd2921b3c17d9413385c3863c6e156b
robertdev <robertdev18@gmail.com>
2025-02-10 20:49:25 -0500

    nav-bar 7

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ class Groomingfurryangels{
     add_theme_support('wp-block-styles');
     add_theme_support('responsive-embeds');
     add_theme_support('editor-styles');
-    add_filter('nav_menu_link_attributes','filter_a_nav',1,3);
+    add_filter('nav_menu_link_attributes',[$this,'filter_a_nav'],1,3);
   }
 
   public function filter_a_nav($atts,$item, $args){
