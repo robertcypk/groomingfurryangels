7872e1818920bc43fee27cc776d4bbc98f707aae
robertdev <robertdev18@gmail.com>
2025-02-10 23:26:49 -0500

    fix nav menu

--- functions.php
+++ functions.php
@@ -20,13 +20,11 @@ class Groomingfurryangels{
     add_theme_support('wp-block-styles');
     add_theme_support('responsive-embeds');
     add_theme_support('editor-styles');
-    add_action('wp_nav_menu',[$this,'filter_a_nav'],10,4);
+    add_filter('wp_nav_menu_items',[$this,'filter_a_nav'],10,4);
     add_action('the_custom_logo',[$this,'website_logo'],1,4);
   }
 
   public function filter_a_nav($attrs,$args){
-    echo 'sss';
-    print_r($attrs);
     if(isset($args->add_a_class)){
       $attrs['link_class'] = $args->add_a_class;
     }
