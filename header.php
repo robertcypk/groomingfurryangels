de182edb6ffe0a7a29688c090dbf3c10d7ecd25b
robertdev <robertdev18@gmail.com>
2025-02-11 20:57:06 -0500

    fix nav menu29

--- header.php
+++ header.php
@@ -32,21 +32,30 @@
 wp_nav_menu(array(
   'container_id' => 'navbar-default',
   'container_class' => 'hidden w-full md:block md:w-auto', 
-  'menu_class' => 'font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700',
+  'menu_class' => 'font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 
+  rounded-lg bg-gray-50 md:flex-row md:space-x8 
+  rtl:space-x-reverse md:mt-0 md:border-0 
+  md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700',
   'theme_location' => 'primary',
   'li_class' => '',
   'fallback_cb' => false,
-  'filter_a_nav_class' => 'block py-2 px-3 text-white bg-blue-700 rounded-sm hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+  'filter_a_nav_class' => 'block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent 
+  md:border-0 md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500 md:dark:hover:bg-gray-700 dark:hover:text-white md:dark:bg-transparent'
 ));
 ?>
       <!--
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 
+                 rounded-lg bg-gray-50 md:flex-row md:space-x-8 
+                 rtl:space-x-reverse md:mt-0 md:border-0 
+                 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
 <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
         </li>
         <li>
           <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 
-md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 
+md:hover:bg-transparent 
+md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white 
+md:dark:hover:text-blue-500 
 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
         </li>
         <li>
