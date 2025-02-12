10a1fc9dfa696b5713794bfccb585abe2f021063
robertdev <robertdev18@gmail.com>
2025-02-11 20:51:45 -0500

    fix nav menu26

--- header.php
+++ header.php
@@ -36,7 +36,7 @@ wp_nav_menu(array(
   'theme_location' => 'primary',
   'li_class' => '',
   'fallback_cb' => false,
-  'filter_a_nav_class' => 'block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+  'filter_a_nav_class' => 'block py-2 px-3 text-white bg-blue-700 rounded-sm hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
 ));
 ?>
       <!--
@@ -45,7 +45,9 @@ wp_nav_menu(array(
 <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 
+md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 
+dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
         </li>
         <li>
           <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
