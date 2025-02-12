d9aa06ad688f4fe7ab7aa1901ba6be2e0d7505f1
robertdev <robertdev18@gmail.com>
2025-02-11 21:01:01 -0500

    fix nav menu31

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ class Groomingfurryangels{
 
   public function website_logo(){
     $image = wp_get_attachment_image_src(get_theme_mod('custom_logo',21),'full');
-    return esc_url($image[0]);
+    return $image[0];
   }
 
   private function getpathscript($path){
